626d09991996f0922ee1c3c342264b82c799d85e
Gert Drapers <gert.drapers@live.com>
2021-04-15 09:55:12 -0700

    Update aserto-cli.rb

--- aserto-cli.rb
+++ aserto-cli.rb
@@ -1,6 +1,3 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class AsertoCli < Formula
   desc "Aserto CLI"
   homepage "https://github.com/aserto-dev/aserto-cli"
