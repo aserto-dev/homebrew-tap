c7d5b824fae16ec4d045528b4d94e312f14dcf9d
Gert Drapers <gert.drapers@live.com>
2021-04-20 07:01:59 -0700

    v0.0.2

--- aserto.rb
+++ aserto.rb
@@ -5,11 +5,11 @@ class Aserto < Formula
   version "0.0.1"
 
   if OS.mac?
-    url "https://github.com/aserto-dev/aserto-cli/releases/download/v0.0.1/aserto_v0.0.1_darwin_x86_64.zip"
-    sha256 "55353926d161e64eea3544b41709a3bb7dfa35e25ca558376f200b0081c727b5"
+    url "https://github.com/aserto-dev/aserto-cli/releases/download/v0.0.2/aserto_v0.0.2_darwin_x86_64.zip"
+    sha256 "f3a0ddb66d717c4fba63cb86186221383f5173179fbafe723ab0f89b5402c35e"
   elsif OS.linux?
-    url "https://github.com/aserto-dev/aserto-cli/releases/download/v0.0.1/aserto_v0.0.1_linux_x86_64.zip"
-    sha256 "fb87ae83e56c82d367e0dc7c42546474b7a10c1ae3da77e3db449915db7a2f00"
+    url "https://github.com/aserto-dev/aserto-cli/releases/download/v0.0.2/aserto_v0.0.2_linux_x86_64.zip"
+    sha256 "05af2a7c4589d9f6931f13f4286b4374f77625a4b4cf07d6de05b187eb880b83"
   end
 
   bottle :unneeded
