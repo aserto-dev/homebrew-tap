9d0434a251e5f05b07511500f3e45c5007b7d04f
Gert Drapers <gert.drapers@live.com>
2021-04-20 07:17:10 -0700

    v0.0.2

--- aserto.rb
+++ aserto.rb
@@ -5,10 +5,10 @@ class Aserto < Formula
   version "0.0.2"
 
   if OS.mac?
-    url "https://github.com/aserto-dev/aserto-cli/releases/download/v0.0.2/aserto_v0.0.2_darwin_x86_64.zip"
+    url "https://github.com/aserto-dev/aserto-cli/releases/download/v0.0.2/aserto_cli_v0.0.2_darwin_x86_64.zip"
     sha256 "f3a0ddb66d717c4fba63cb86186221383f5173179fbafe723ab0f89b5402c35e"
   elsif OS.linux?
-    url "https://github.com/aserto-dev/aserto-cli/releases/download/v0.0.2/aserto_v0.0.2_linux_x86_64.zip"
+    url "https://github.com/aserto-dev/aserto-cli/releases/download/v0.0.2/aserto_cli_v0.0.2_linux_x86_64.zip"
     sha256 "05af2a7c4589d9f6931f13f4286b4374f77625a4b4cf07d6de05b187eb880b83"
   end
 
