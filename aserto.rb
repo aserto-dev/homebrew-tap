2e95818ba932b23e06d1d94ffe1d15cbc7bc63ec
Gert Drapers <gert.drapers@live.com>
2021-05-06 19:12:48 -0700

    Brew formula update for aserto version v0.0.13

--- aserto.rb
+++ aserto.rb
@@ -2,14 +2,14 @@ class Aserto < Formula
   desc "Aserto CLI"
   homepage "https://github.com/aserto-dev/aserto-cli"
   license "MIT"
-  version "0.0.9"
+  version "0.0.13"
 
   if OS.mac?
-    url "https://github.com/aserto-dev/aserto-cli/releases/download/v0.0.9/aserto_0.0.9_darwin_x86_64.zip"
-    sha256 "b88b2d5ee8a9c424f400156e371443dda7f561e1591c68c5542d3f13542e1019"
+    url "https://github.com/aserto-dev/aserto-cli/releases/download/v0.0.13/aserto_0.0.13_darwin_x86_64.zip"
+    sha256 "5595aecd699e28aeb572ce02dd40efb0d4de181ac428e9818ad8fa3ce39a5e5b"
   elsif OS.linux?
-    url "https://github.com/aserto-dev/aserto-cli/releases/download/v0.0.9/aserto_0.0.9_linux_x86_64.zip"
-    sha256 "05d5b6238213684c298c265b5ae90aa52b85cac53d43c909d5776c5a886911c4"
+    url "https://github.com/aserto-dev/aserto-cli/releases/download/v0.0.13/aserto_0.0.13_linux_x86_64.zip"
+    sha256 "f0d049de3e7f7b31eb20fff69586b76a6acf99d4cbadc812106bf23d1c8f1445"
   end
 
   bottle :unneeded
